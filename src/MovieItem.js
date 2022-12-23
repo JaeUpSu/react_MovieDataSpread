36df731017c7f2e822a3b9067b08016de6263356
jaeupsu <hyeongs2323@gmail.com>
2022-12-23 13:22:59 +0900

    docs: README 기능목록 작성

---
+++ src/MovieItem.js
@@ -0,0 +1,5 @@
+function MovieItem(props) {
+  return <div></div>;
+}
+
+export default MovieItem;
---
+++ src/MovieList.js
@@ -0,0 +1,7 @@
+import { userEffect, useState } from "react";
+
+function MovieList() {
+  return <div></div>;
+}
+
+export default MovieList;
